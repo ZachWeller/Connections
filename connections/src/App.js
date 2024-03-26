73ea232d9fa0d432a997084e4c5bdb1982b56d57
ZachWeller <zachwellerdev@gmail.com>
2024-03-26 16:16:03 -0400

    Layout Done

---
+++ connections/src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="text-5xl font-extralight">Groups</div>
+      <div className="grid place-content-center grid-cols-4 grid-rows-4 mt-20 gap-2 ">
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400 ">1</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">2</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">3</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">4</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">5</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">6</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">7</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">8</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">9</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">10</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">11</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">12</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">13</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">14</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">15</div>
+        <div className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-400">16</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
