9156475167970db85dd9c2f9cd66d96f30852cbb
ZachWeller <zachwellerdev@gmail.com>
2024-04-03 17:52:33 -0400

    80%

--- connections/src/App.js
+++ connections/src/App.js
@@ -12,47 +12,49 @@ import {useEffect, useState} from "react";
 // Future additions: Timer,
 
 function App() {
-  const [userObjects, setUserObjects] = useState([]);
-  const [userList, setUserList] = useState([]);
-  const [wordsObjects, setWordsObjects] = useState([]);
-  const [wordsArray, setWordsArray] = useState([]);
-  let lives = 4;
-
+  const [objectsOfUserSelectedWords, setObjectsOfUserSelectedWords] = useState([]);
+  const [arrayOfUserSelectedWords, setArrayOfUserSelectedWords] = useState([]);
+  const [objectsContainingWordsAndReason, setObjectsContainingWordsAndReasons] = useState([]);
+  const [arrayOfDisplayWords, setArrayOfDisplayWords] = useState([]);
+  const [amountOfMatches, setAmountOfMatches] = useState(0);
+  const [arrayOfMatches, setArrayOfMatches] = useState([]);
+  const [lives, setLives] = useState(4);
+  console.log(arrayOfUserSelectedWords);
   useEffect(() => {
     axios.get("http://localhost:5000/connections/words").then((response) => {
-      setWordsObjects(response.data);
+      setObjectsContainingWordsAndReasons(response.data);
       // reduces the response to gather the accumulative and then create array filled with just the words
       const allWords = response.data.reduce((acc, obj) => acc.concat(obj.words), []);
-      setWordsArray(shuffleArray(allWords));
+      setArrayOfDisplayWords(shuffleArray(allWords));
     });
   }, []);
 
   const handleUserAddition = (value, index, e) => {
-    // Div of the clicked box
     let box = document.getElementById(index);
 
     // If the user has not filled the array and the value is not inside of the array, add it.
-    if (userList.length <= 3 && !userList.includes(value)) {
-      setUserList((prev) => [...prev, value]);
-      setUserObjects((prev) => [
+    if (arrayOfUserSelectedWords.length <= 3 && !arrayOfUserSelectedWords.includes(value)) {
+      setArrayOfUserSelectedWords((prev) => [...prev, value]);
+      setObjectsOfUserSelectedWords((prev) => [
         ...prev,
         {
           word: value,
           id: index,
         },
       ]);
+
       box.style.backgroundColor = "rgb(156 163 175 / 1)";
-    } else if (userList.includes(value)) {
+    } else if (arrayOfUserSelectedWords.includes(value)) {
       // Finds the object index that contains the value
-      let index = userObjects
+      let index = objectsOfUserSelectedWords
         .map((obj) => {
           return obj.word;
         })
         .indexOf(value);
 
       // Since the index is the same for both useStates, reuse the same index to remove the value
-      userObjects.splice(index, 1);
-      userList.splice(index, 1);
+      objectsOfUserSelectedWords.splice(index, 1);
+      arrayOfUserSelectedWords.splice(index, 1);
 
       box.style.backgroundColor = "rgb(209 213 219 / 1)";
     } else {
@@ -62,6 +64,8 @@ function App() {
 
   function shuffleArray(array) {
     // Created a new variable so that the useState is rerendered when shuffled
+    handleDeselectAll();
+
     const shuffledArray = [...array];
     for (var i = array.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -73,59 +77,224 @@ function App() {
   }
 
   // Grid of boxes
-  const displayWords = (event) => {
-    return (
-      <>
-        {wordsArray.map((item, index) => (
-          <div
-            className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-500 "
-            onClick={(e) => handleUserAddition(item, index, e)}
-            key={index}
-            id={index}
-          >
-            {item}
+  const board = () => {
+    if (amountOfMatches === 0) {
+      return (
+        <div className="grid place-content-center grid-cols-4 grid-rows-4 gap-2">
+          {arrayOfDisplayWords.map((item, index) => (
+            <div
+              className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer max-w-full hover:bg-gray-500 "
+              onClick={(e) => handleUserAddition(item, index, e)}
+              key={index}
+              id={index}
+            >
+              {item}
+            </div>
+          ))}
+        </div>
+      );
+    }
+    if (amountOfMatches === 1) {
+      return (
+        <>
+          <div className="bg-yellow-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+            <div className="font-bold text-lg">{arrayOfMatches[0].reason.toUpperCase()}</div>
+            <div className="flex flex-row gap-1">
+              {arrayOfMatches[0].words.map((word, idx) => (
+                <div className="font-semibold">{idx === arrayOfMatches[0].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+              ))}
+            </div>
           </div>
-        ))}
-      </>
-    );
+          <div className="grid place-content-center grid-cols-4 grid-rows-3 gap-2">
+            {arrayOfDisplayWords.map((item, index) => (
+              <div
+                className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-500 "
+                onClick={(e) => handleUserAddition(item, index, e)}
+                key={index}
+                id={index}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </>
+      );
+    }
+    if (amountOfMatches === 2) {
+      return (
+        <>
+          <div className="bg-yellow-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+            <div className="font-bold text-lg">{arrayOfMatches[0].reason.toUpperCase()}</div>
+            <div className="flex flex-row gap-1">
+              {arrayOfMatches[0].words.map((word, idx) => (
+                <div className="font-semibold">{idx === arrayOfMatches[0].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+              ))}
+            </div>
+          </div>
+          <div className="bg-green-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+            <div className="font-bold text-lg">{arrayOfMatches[1].reason.toUpperCase()}</div>
+            <div className="flex flex-row gap-1">
+              {arrayOfMatches[0].words.map((word, idx) => (
+                <div className="font-semibold">{idx === arrayOfMatches[1].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+              ))}
+            </div>
+          </div>
+          <div className="grid place-content-center grid-cols-4 grid-rows-2 gap-2">
+            {arrayOfDisplayWords.map((item, index) => (
+              <div
+                className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-500 "
+                onClick={(e) => handleUserAddition(item, index, e)}
+                key={index}
+                id={index}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </>
+      );
+    }
+    if (amountOfMatches === 3) {
+      return (
+        <>
+          <div className="bg-yellow-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+            <div className="font-bold text-lg">{arrayOfMatches[0].reason.toUpperCase()}</div>
+            <div className="flex flex-row gap-1">
+              {arrayOfMatches[0].words.map((word, idx) => (
+                <div className="font-semibold">{idx === arrayOfMatches[0].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+              ))}
+            </div>
+          </div>
+          <div className="bg-green-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+            <div className="font-bold text-lg">{arrayOfMatches[1].reason.toUpperCase()}</div>
+            <div className="flex flex-row gap-1">
+              {arrayOfMatches[0].words.map((word, idx) => (
+                <div className="font-semibold">{idx === arrayOfMatches[1].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+              ))}
+            </div>
+          </div>
+          <div className="bg-blue-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+            <div className="font-bold text-lg">{arrayOfMatches[2].reason.toUpperCase()}</div>
+            <div className="flex flex-row gap-1">
+              {arrayOfMatches[0].words.map((word, idx) => (
+                <div className="font-semibold">{idx === arrayOfMatches[2].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+              ))}
+            </div>
+          </div>
+          <div className="grid place-content-center grid-cols-4 grid-rows-1 gap-2">
+            {arrayOfDisplayWords.map((item, index) => (
+              <div
+                className="w-28 bg-gray-300 rounded-md aspect-square grid place-items-center cursor-pointer hover:bg-gray-500 "
+                onClick={(e) => handleUserAddition(item, index, e)}
+                key={index}
+                id={index}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
+        </>
+      );
+    }
+    if (amountOfMatches === 4) {
+      return (
+        <>
+          <div className="grid place-content-center grid-cols-1 grid-rows-4 gap-2">
+            <div className="bg-yellow-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center px-20">
+              <div className="font-bold text-lg">{arrayOfMatches[0].reason.toUpperCase()}</div>
+              <div className="flex flex-row gap-1">
+                {arrayOfMatches[0].words.map((word, idx) => (
+                  <div className="font-semibold">{idx === arrayOfMatches[0].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+                ))}
+              </div>
+            </div>
+            <div className="bg-green-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+              <div className="font-bold text-lg">{arrayOfMatches[1].reason.toUpperCase()}</div>
+              <div className="flex flex-row gap-1">
+                {arrayOfMatches[0].words.map((word, idx) => (
+                  <div className="font-semibold">{idx === arrayOfMatches[1].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+                ))}
+              </div>
+            </div>
+            <div className="bg-blue-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+              <div className="font-bold text-lg">{arrayOfMatches[2].reason.toUpperCase()}</div>
+              <div className="flex flex-row gap-1">
+                {arrayOfMatches[0].words.map((word, idx) => (
+                  <div className="font-semibold">{idx === arrayOfMatches[2].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+                ))}
+              </div>
+            </div>
+            <div className="bg-purple-400 h-28 rounded-md mb-2 flex flex-col justify-center items-center">
+              <div className="font-bold text-lg">{arrayOfMatches[3].reason.toUpperCase()}</div>
+              <div className="flex flex-row gap-1">
+                {arrayOfMatches[0].words.map((word, idx) => (
+                  <div className="font-semibold">{idx === arrayOfMatches[3].words.length - 1 ? word.toUpperCase() : `${word.toUpperCase()}, `}</div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    }
   };
 
   const handleSubmission = () => {
-    if (userList.length === 4) {
-      // Compare userList to each WordObjects
-      wordsObjects.forEach((obj) => {
-        // console.log(obj.words);
-        console.log(userList == obj.words);
-        if (userList.includes(obj.words)) {
-          console.log("SUCCESS");
-        } else {
-          console.log(obj.words, userList);
+    if (arrayOfUserSelectedWords.length === 4) {
+      // For each object we set a correct count to start at 0, after we loop through each word array within
+      // the object and we see if that word is within the user list and then increase the correct count
+      // Short term have a correct count for each word array and check if each word is within the user list
+      let success = false;
+      objectsContainingWordsAndReason.forEach((obj) => {
+        let correctCount = 0;
+        obj.words.forEach((word) => {
+          if (arrayOfUserSelectedWords.includes(word)) {
+            correctCount++;
+          }
+        });
+        if (correctCount === 4) {
+          success = true;
+          setAmountOfMatches(amountOfMatches + 1);
+          arrayOfUserSelectedWords.forEach((word) => {
+            let index = arrayOfDisplayWords.indexOf(word);
+            arrayOfDisplayWords.splice(index, 1);
+          });
+          setArrayOfUserSelectedWords([]);
+          setArrayOfMatches((prev) => [...prev, obj]);
+        }
+        if (correctCount === 3) {
+          console.log("One away!");
         }
       });
-      // If user is wrong, remove a life
-      // If user is off by one, tell the user otherwise say shake selected boxes and remove a life
-      // If user is correct, merge boxes and move to the top and display the reason
-      return;
+
+      if (success === false) {
+        setLives(lives - 1);
+      }
     } else {
+      // TODO
       // Tell user to add another one
+      console.log("add more");
     }
   };
 
   // Grabs each index within the userObject and removes them, also removes the styling of each box
   const handleDeselectAll = () => {
-    setUserObjects([]);
-    setUserList([]);
-    userObjects.forEach((item) => {
+    setObjectsOfUserSelectedWords([]);
+    setArrayOfUserSelectedWords([]);
+    objectsOfUserSelectedWords.forEach((item) => {
       document.getElementById(item.id).style.backgroundColor = "rgb(209 213 219 / 1)";
     });
   };
+
   return (
     <>
-      {lives > 0 && (
-        <div className="h-screen flex flex-col items-center justify-center gap-y-10">
-          <div className="text-5xl font-extralight">Groups</div>
-
-          <div className="grid place-content-center grid-cols-4 grid-rows-4 gap-2">{wordsObjects.length > 0 && displayWords()}</div>
+      <div className="h-screen flex flex-col items-center justify-center gap-y-10">
+        <div className="text-5xl font-extralight">Groups</div>
+        <div className="max-w-screen-sm px-4">{objectsContainingWordsAndReason.length > 0 && lives > 0 && board()}</div>
+        <div className="grid text-center">
+          <div>Lives</div>
+          <div>[****]</div>
+        </div>
+        {arrayOfMatches.length < 4 && (
           <div className="flex gap-x-4">
             <button
               className="border-2 border-gray-500 rounded-full px-3 py-2 font-semibold text-gray-800 hover:bg-gray-500 hover:text-white hover:transition-colors"
@@ -141,13 +310,18 @@ function App() {
             </button>
             <button
               className="border-2 border-gray-500 rounded-full px-3 py-2 font-semibold text-gray-800 hover:bg-gray-500 hover:text-white hover:transition-colors"
-              onClick={() => setWordsArray(shuffleArray(wordsArray))}
+              onClick={() => setArrayOfDisplayWords(shuffleArray(arrayOfDisplayWords))}
             >
               Shuffle
             </button>
           </div>
-        </div>
-      )}
+        )}
+        {arrayOfMatches.length === 4 && (
+          <button className="border-2 border-gray-500 rounded-full px-3 py-2 font-semibold text-gray-800 hover:bg-gray-500 hover:text-white hover:transition-colors">
+            Back
+          </button>
+        )}
+      </div>
     </>
   );
 }
